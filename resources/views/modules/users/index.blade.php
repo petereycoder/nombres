6aec3c1f00d0de1fe3dc5cfbd6ea2b08139cf471
petereycoder <fp.pipe.reyes@gmail.com>
2024-10-07 16:36:12 -0600

    Creación del proyecto

---
+++ resources/views/modules/users/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('contenido')
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col">
+                <h2>Crud Laravel 11</h2>
+                <div class="card">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
